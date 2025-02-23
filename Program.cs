530d41708a162e944ddb7374c536f35ca2750d35
stevrasjid <castronomiakun@gmail.com>
2025-02-23 14:56:20 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,13 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddControllers();  // Register controllers
-
-// // Add services to the container.
-// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-
-// Add CORS Policy (Allow All)
+builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -20,7 +13,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
